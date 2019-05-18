0368f210e809b447f239cd4b336c2740934b6e70
SHxKM <shibelkm@gmail.com>
2019-05-19 00:03:57 +0300

    made some additions

--- {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/scratch.py
+++ {{ cookiecutter.project_slug }}/{{ cookiecutter.project_slug }}/scratch.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python
 import os
 import sys
+
 # needed for Sublime Text 3
-# sys.path.append('/Users/shibel/PycharmProjects/{{ cookiecutter.project_slug }}')
+# sys.path.append('PATHTOPROJECT/{{ cookiecutter.project_slug }}')
 # set the Django settings module before initializing Django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_slug }}.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "{{ cookiecutter.project_slug }}.settings"
+)
 import django
+
 django.setup()
